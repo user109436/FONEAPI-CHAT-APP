490d796d2d3885ebccf19eb25d621fd20415b809
user109436 <zedekfelix7@gmail.com>
2022-06-05 01:55:28 +0800

    SVG 404 not found

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -20,13 +20,6 @@ const NotFound = () => (
             flexDirection: "column",
           }}
         >
-          <Typography align="center" color="textPrimary" variant="h1">
-            404: The page you are looking for isn’t here
-          </Typography>
-          <Typography align="center" color="textPrimary" variant="subtitle2">
-            You either tried some shady route or you came here by mistake.
-            Whichever it is, try using the navigation
-          </Typography>
           <Box sx={{ textAlign: "center" }}>
             <img
               alt="Under development"
