0da34cbf8c2ced2605fb0119c75967df8773286b
user109436 <zedekfelix7@gmail.com>
2022-06-05 15:29:40 +0800

    ENDPOINT:Identify User By JWT Token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
+const { promisify } = require("util");
 const factory = require("./handlerFactory");
 const User = require("../models/userModel");
-
+const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 //CRUD
 exports.createUser = factory.createOne(User);
 exports.getUsers = factory.getAll(User);
@@ -9,3 +11,21 @@ exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
 
 //unique query operations
+exports.identifyUserByJWT = catchAsync(async (req, res, next) => {
+  const decoded = await promisify(jwt.verify)(
+    req.body.token,
+    process.env.JWT_SECRET
+  );
+  const doc = await User.findOne({ _id: decoded.id });
+  if (!doc) {
+    //log out user
+    return res.status(401).json({
+      status: "fail",
+      message: "Token Malformed",
+    });
+  }
+  return res.status(200).json({
+    status: "success",
+    doc,
+  });
+});
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
 
+router.route("/identify-user").post(userController.identifyUserByJWT);
+
 // API prefix is located at app.js
 router.use(authController.protect);
 router.route("/").post(userController.createUser).get(userController.getUsers);
